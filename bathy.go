d750584704ef260f3fdc7a623fa7b0fd44311930
Fu Xiang <20948660@qq.com>
2016-11-26 00:12:34 +0800

    continue add structure

--- bathy.go
+++ bathy.go
@@ -1,9 +1,7 @@
 // structure
 package SonarRegener
 
-import (
 //"fmt"
-)
 
 type Bathy struct {
 	Header         OIC_Header
@@ -16,6 +14,43 @@ type Bathy struct {
 //initialize bathy struct ,call OIC initialize
 func (bathy *Bathy) Init() {
 	OICInit(header, isbathy)
+	bathy.Header.ChanNum = 5
+	bathy.Header.ChanOffset[0] = 0
+	bathy.Header.ChanOffset[1] = 620
+	bathy.Header.ChanOffset[2] = 1240
+	bathy.Header.ChanOffset[3] = 2440
+	bathy.Header.ChanOffset[4] = 3640
+	bathy.Header.Channel[0].Type = 7
+	bathy.Header.Channel[0].Side = 0
+	bathy.Header.Channel[0].Size = 3 //float32
+	bathy.Header.Channel[0].Empt = 0
+	bathy.Header.Channel[0].Frequency = 12
+	bathy.Header.Channel[0].Samples = 155
+	bathy.Header.Channel[1].Type = 7 //8?
+	bathy.Header.Channel[1].Side = 0
+	bathy.Header.Channel[1].Size = 3 //float32
+	bathy.Header.Channel[1].Empt = 0
+	bathy.Header.Channel[1].Frequency = 12
+	bathy.Header.Channel[1].Samples = 155
+	bathy.Header.Channel[2].Type = 7
+	bathy.Header.Channel[2].Side = 1
+	bathy.Header.Channel[2].Size = 3 //float32
+	bathy.Header.Channel[2].Empt = 0
+	bathy.Header.Channel[2].Frequency = 12
+	bathy.Header.Channel[2].Samples = 300
+	bathy.Header.Channel[3].Type = 7
+	bathy.Header.Channel[3].Side = 1
+	bathy.Header.Channel[3].Size = 3 //float32
+	bathy.Header.Channel[3].Empt = 0
+	bathy.Header.Channel[3].Frequency = 12
+	bathy.Header.Channel[3].Samples = 155
+	bathy.Header.Channel[4].Type = 9
+	bathy.Header.Channel[4].Side = 0
+	bathy.Header.Channel[4].Size = 1 //int16
+	bathy.Header.Channel[4].Empt = 0
+	bathy.Header.Channel[4].Frequency = 12
+	bathy.Header.Channel[4].Samples = 56
+
 }
 
 func (bathy *Bathy) Parse(recvbuf []int8) error {
