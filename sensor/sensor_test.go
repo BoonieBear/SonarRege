877310624745c9b4920d71a8099c585247d4ae61
Fu Xiang <20948660@qq.com>
2017-02-07 23:23:06 +0800

    add queue watch test case

--- sensor/sensor_test.go
+++ sensor/sensor_test.go
@@ -12,22 +12,23 @@ func TestNewQueue(t *testing.T) {
 		fmt.Println(q)
 	} else {
 		fmt.Printf("Queue size=%d\n", q.size)
-		t.Error("NewQueue Failed!")
+		t.Fatal("NewQueue Failed!")
 	}
 }
 
 func TestPushAndPop(t *testing.T) {
 	q := NewQueue(10)
 
-	for i := 1; i < 10; i++ {
+	for i := 0; i < 10; i++ {
 		p := &node{
 			Time: time.Now(),
+			Data: &sensor1{value1: 12, value2: 243},
 		}
 		q.Push(p)
 	}
 
 	if q.count != 10 {
-		t.Error("Push Failed!")
+		t.Fatalf("Push Failed,count = %d", q.count)
 	}
 	n := q.Pop()
 	if n != nil && q.count == 9 {
@@ -35,6 +36,30 @@ func TestPushAndPop(t *testing.T) {
 	} else {
 		fmt.Printf("Queue size=%d\n", q.size)
 		fmt.Println(q)
-		t.Error("Pop Failed!")
+		t.Fatal("Pop Failed!")
 	}
 }
+func TestWatch(t *testing.T) {
+	q := NewQueue(10)
+
+	p := &node{
+		Time: time.Now(),
+		Data: &sensor1{value1: 12, value2: 243},
+	}
+	q.Push(p)
+
+	if q.count != 1 {
+		t.Fatalf("Push Failed,count = %d", q.count)
+	}
+	n := q.Watch()
+	if n != nil && q.count == 1 {
+		fmt.Println(n)
+	} else {
+		fmt.Printf("Queue size=%d\n", q.size)
+		fmt.Println(q)
+		t.Fatal("Watch Failed!")
+	}
+}
+func TestFetchData(t *testing.T）｛
+
+｝
